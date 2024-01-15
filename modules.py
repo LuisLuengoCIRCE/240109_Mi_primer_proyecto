090f7107b6aa73316c26cd769eda5e3c6d1d4fe8
LuisLuengoCIRCE <153526114+LuisLuengoCIRCE@users.noreply.github.com>
2024-01-15 15:26:46 +0100

    Update modules.py
    change BESS params

--- modules.py
+++ modules.py
@@ -3,8 +3,7 @@ class Modules:
     def __init__(self):
         self.voltage = 3
         self.rated_capacity = 4100
-        self.soc = 0.5
+        self.soc = 0.9
         self.soh = 1
         self.temperature = 25
         self.current = 0
-        self.voltage = 3
